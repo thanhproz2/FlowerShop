37814e2ceaf6061f8323e77b936f404a58f9bd79
Thanh Tran Duc <ducthanh448@gmail.com>
2017-10-14 09:37:43 +0700

    update database, load new Product, load feature product

--- FlowerShop/FlowerShop/Controllers/HomeController.cs
+++ FlowerShop/FlowerShop/Controllers/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FlowerShop.Models;
 
 namespace FlowerShop.Controllers
 {
     public class HomeController : Controller
     {
+        FlowerShopEntities db = new FlowerShopEntities();
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.fPro = db.Products.Where(p => p.status == true).ToList();
+            ViewBag.nPro = db.Products.Where(p => p.specials == true).ToList();
             return View();
         }
     }
